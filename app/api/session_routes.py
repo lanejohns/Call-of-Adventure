623d0ba70df287e53c3442e8bfa7548550c8aac2
Lane <lanedaltonjohns@gmail.com>
2021-03-03 12:35:37 -0500

    finish routes for session

--- app/api/session_routes.py
+++ app/api/session_routes.py
@@ -11,3 +11,40 @@ def all_sessions():
     return {"all_sessions": {session.id: session.to_dict() for session in sessions}}
 
 
+@session_routes.route('/', methods=["POST"])
+def create_session():
+    form = SessionForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        session = Session(
+            party_id=1,
+            title=form.data['title'],
+            description=form.data['description'],
+            date=form.data['date'],
+            time=form.data['time'],
+            address=form.data['address'],
+            city=form.data['city'],
+            state=form.data['state'],
+            zipcode=form.data['zipcode'],
+            latitude=form.data['latitude'],
+            longitude=form.data['longitude'],
+            in_person=form.data['in_person']
+        )
+        db.session.add(session)
+        db.session.commit()
+        return {"session": session.to_dict()}
+    return {"errors": "set errors here"}
+
+
+@session_routes.route('/<id>', methods=['DELETE'])
+def delete_session(id):
+    session = Session.query.filter(Session.id == id).first()
+    db.session.delete(session)
+    db.session.commit()
+    return {'session': session.to_dict()}
+
+
+@session_routes.route('/<id>')
+def single_session(id):
+    session = Session.query.get(id)
+    return {'session': session.to_dict()}
