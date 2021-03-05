0e2b438ebf6ed479794a7d132f4ef01a5f2d66c2
Lane <lanedaltonjohns@gmail.com>
2021-03-05 09:00:43 -0500

    started CreateSessionComponent and can conditionally render it in PartyProfile component

--- react-app/src/components/PartyProfileComponent/index.js
+++ react-app/src/components/PartyProfileComponent/index.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
 
 import { getParty } from "../../store/party"
+import CreateSessionComponent from "../SessionComponent/CreateSessionComponent/index"
 import "./PartyProfile.css"
 
 const PartyProfileComponent = () => {
@@ -11,6 +12,12 @@ const PartyProfileComponent = () => {
     const party = useSelector(state => state.parties.party)
     const partyId = Number.parseInt(useParams().partyId)
 
+    const [createSession, setCreateSession] = useState(false)
+
+    const handleClick = () => {
+        setCreateSession(!createSession)
+    }
+
     useEffect(() => {
         dispatch(getParty(partyId))
     }, [dispatch])
@@ -18,7 +25,13 @@ const PartyProfileComponent = () => {
     return (
         <div>
             {party && 
-            <h1>{party.party_name}</h1>
+            <div>
+                <h1>{party.party_name}</h1>
+                <button onClick={handleClick}>Make a session</button>
+            </div>
+            }
+            {createSession == true && 
+            <CreateSessionComponent />
             }
         </div>
     )
---
+++ react-app/src/components/SessionComponent/CreateSessionComponent/index.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { useParams } from "react-router-dom"
+
+const CreateSessionComponent = () => {
+    return (
+        <h1>This is the Session Creation Component</h1>
+    )
+}
+
+export default CreateSessionComponent
